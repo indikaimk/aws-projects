69e6338923a8293447b755bc0bc7f5035fbf3a1b
cloudqubes <cloudqubes@gmail.com>
2024-10-08 16:19:53 +0530

    a-web-app

--- a-webapp/lib/a-webapp-stack.ts
+++ a-webapp/lib/a-webapp-stack.ts
@@ -1,16 +1,82 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { constants } from 'fs/promises';
+import { Alias } from 'aws-cdk-lib/aws-kms';
+import { TargetTrackingScalingPolicy } from 'aws-cdk-lib/aws-applicationautoscaling';
+import { AlbTarget } from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets';
+import { LoadBalancerTarget } from 'aws-cdk-lib/aws-route53-targets';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AWebappStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'a-webapp-vpc', {
+      natGateways: 0,
+      ipAddresses: ec2.IpAddresses.cidr('172.30.0.0/16'),
+      ipProtocol: ec2.IpProtocol.DUAL_STACK,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AWebappQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const albSG = new ec2.SecurityGroup(this, "alb-sg", {
+      vpc: vpc
+    });
+
+    const appServerSG = new ec2.SecurityGroup(this, "app-server-sg", {
+      vpc: vpc
+    })
+
+    albSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.HTTP);
+    albSG.addIngressRule(ec2.Peer.anyIpv6(), ec2.Port.HTTP);
+    albSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.HTTPS);
+    albSG.addIngressRule(ec2.Peer.anyIpv6(), ec2.Port.HTTPS);
+    albSG.addEgressRule(ec2.Peer.securityGroupId(appServerSG.uniqueId), ec2.Port.tcp(8080))
+
+    appServerSG.addIngressRule(ec2.Peer.securityGroupId(albSG.uniqueId), ec2.Port.tcp(8080))
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, "a-webapp-alb", {
+      vpc: vpc,
+      internetFacing: true,
+      securityGroup: albSG
+    });
+
+    const port80Listner = alb.addListener('a-webapp-port-80', {
+      port: 80
+    });
+
+    const port443Listner = alb.addListener('a-webapp-port-443', {
+      port: 443
+    });
+
+    const aWebappZone = new route53.HostedZone(this, "a-webapp-hosted-zone", {
+      zoneName: "liyumapp.com"
+    })
+
+    const aRecord = new route53.ARecord(this, 'webapp-a-record', {
+      zone: aWebappZone,
+      recordName: 'dev',
+      target: route53.RecordTarget.fromAlias(new AlbTarget.)
+    })
+
+    const appServerTemplate = new ec2.LaunchTemplate(this, 'app-server-template', {
+      machineImage: ec2.MachineImage.genericLinux({"us-east-2": "ami-03bfe38a90ce33425"}),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      securityGroup: appServerSG
+    });
+
+    const appServerASG = new autoscaling.AutoScalingGroup(this, 'app-server-asg', {
+      vpc: vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+      },
+      desiredCapacity: 1,
+      minCapacity: 0,
+      maxCapacity: 2,
+      launchTemplate: appServerTemplate,
+    });
   }
 }
